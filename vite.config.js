705a381d727ef44dd4d8c577664d7de85c10bed4
pineapplegoose <teniolakalaro@gmail.com>
2023-06-07 17:03:43 +0100

    new commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
+import imageminPlugin from 'vite-plugin-imagemin';
 import react from '@vitejs/plugin-react';
 import sassPlugin from 'vite-plugin-sass';
 
 export default defineConfig({
   plugins: [
     react(),
-    sassPlugin()
+    sassPlugin(),
+    imageminPlugin({
+      gifsicle: { optimizationLevel: 7, interlaced: false, colors: 256 },
+      optipng: { optimizationLevel: 7 },
+      jpegtran: { progressive: true },
+      svgo: {},
+    }),
   ]
 });
